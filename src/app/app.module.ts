bfd68b9a0287c3f246cd88fee5ba3dcb03653b7e
Maciej Wojcik <maciek@testaarosa.pl>
2019-12-29 17:02:51 +0100

    my initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
+import { SearchService } from './service/search.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { SearchReportComponent } from './search-report/search-report.component';
+import { ResultSearchComponent } from './search-report/result-search/result-search.component';
+import { FormSearchComponent } from './search-report/form-search/form-search.component';
+import { BsDatepickerModule } from 'ngx-bootstrap';
+import { AlertModule } from 'ngx-bootstrap';
+import { BrowserAnimationsModule,NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { DatePipe } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchReportComponent,
+    ResultSearchComponent,
+    FormSearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BsDatepickerModule.forRoot(), AlertModule.forRoot(),
+    BrowserAnimationsModule,NoopAnimationsModule
   ],
-  providers: [],
+  providers: [SearchService, DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
